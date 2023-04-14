b94957bdae35c5f6f952b4ccac7cb1107a6ccda6
fadly <fadlypku1@gmail.com>
2023-04-15 00:48:44 +0700

    Tugas PHP

---
+++ aplikasi.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata Diri</title>
+	<style type="text/css">
+		body {
+			background-color: #f2f2f2;
+			font-family: 'Times New Roman', Times, serif, sans-serif;
+		}
+		h1 {
+			text-align: center;
+		}
+		table {
+			margin: auto;
+			border-collapse: collapse;
+			border: 1px solid black;
+		}
+		th, td {
+			padding: 10px;
+			border: 1px solid black;
+		}
+		img {
+			display: block;
+			margin: auto;
+			width: 150px;
+			height: 200px;
+			object-fit: cover;
+			object-position: center;
+			border: 1px solid black;
+			border-radius: 10px;
+			box-shadow: 3px 3px 5px grey;
+		}
+	</style>
+</head>
+<body>
+	<h1>Biodata Diri</h1>
+	<table>
+		<tr>
+			<th>Nama</th>
+			<td>Fadly Indra Agustin</td>
+		</tr>
+		<tr>
+			<th>TTL</th>
+			<td>Batusangkar, 06 Agustus 2001</td>
+		</tr>
+		<tr>
+			<th>Jenis Kelamin</th>
+			<td>Laki-laki</td>
+		</tr>
+		<tr>
+			<th>Alamat</th>
+			<td>Riau, Kota Pekanbaru, Kecamatan Tuah Madani, Kelurahan Sialangmunggu, Jl. Pendidikan, Gang Tauhid II.</td>
+		</tr>
+		<tr>
+			<th>Foto</th>
+			<td><img src="../FSWD1-arkatama-php/assets/IMG-20211030-WA0005.jpg" alt=""></td>
+		</tr>
+		<tr>
+			<th>Riwayat Pendidikan</th>
+			<td>
+				<ul>
+					<li>SDN 21 Tanjung Bonai (2008-2014)</li>
+					<li>SMPN 03 Lintau Buo Utara (2014-2017)</li>
+					<li>SMAN 01 Lintau Buo Utara (2017-2020)</li>
+					<li>STMIK AMIK RIAU, Fakultas Teknik Informatika (2020-sekarang)</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<th>Hobby</th>
+			<td>Ngoding, Bermain Basket, Mendaki Gunung, Bermain Game, Membaca Buku</td>
+		</tr>
+	</table>
+</body>
+</html>
