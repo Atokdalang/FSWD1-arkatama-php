8c6fb91f2ed91df804ecb0db71362308c20ae400
fadly <fadlypku1@gmail.com>
2023-04-15 19:23:34 +0700

    Tugas PHP

--- aplikasi.php
+++ aplikasi.php
@@ -1,75 +1,104 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Biodata Diri</title>
-	<style type="text/css">
-		body {
-			background-color: #f2f2f2;
-			font-family: 'Times New Roman', Times, serif, sans-serif;
-		}
-		h1 {
-			text-align: center;
-		}
-		table {
-			margin: auto;
-			border-collapse: collapse;
-			border: 1px solid black;
-		}
-		th, td {
-			padding: 10px;
-			border: 1px solid black;
-		}
-		img {
-			display: block;
-			margin: auto;
-			width: 150px;
-			height: 200px;
-			object-fit: cover;
-			object-position: center;
-			border: 1px solid black;
-			border-radius: 10px;
-			box-shadow: 3px 3px 5px grey;
-		}
-	</style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Biodara Diri PHP</title>
 </head>
 <body>
-	<h1>Biodata Diri</h1>
-	<table>
-		<tr>
-			<th>Nama</th>
-			<td>Fadly Indra Agustin</td>
-		</tr>
-		<tr>
-			<th>TTL</th>
-			<td>Batusangkar, 06 Agustus 2001</td>
-		</tr>
-		<tr>
-			<th>Jenis Kelamin</th>
-			<td>Laki-laki</td>
-		</tr>
-		<tr>
-			<th>Alamat</th>
-			<td>Riau, Kota Pekanbaru, Kecamatan Tuah Madani, Kelurahan Sialangmunggu, Jl. Pendidikan, Gang Tauhid II.</td>
-		</tr>
-		<tr>
-			<th>Foto</th>
-			<td><img src="../FSWD1-arkatama-php/assets/IMG-20211030-WA0005.jpg" alt=""></td>
-		</tr>
-		<tr>
-			<th>Riwayat Pendidikan</th>
-			<td>
-				<ul>
-					<li>SDN 21 Tanjung Bonai (2008-2014)</li>
-					<li>SMPN 03 Lintau Buo Utara (2014-2017)</li>
-					<li>SMAN 01 Lintau Buo Utara (2017-2020)</li>
-					<li>STMIK AMIK RIAU, Fakultas Teknik Informatika (2020-sekarang)</li>
-				</ul>
-			</td>
-		</tr>
-		<tr>
-			<th>Hobby</th>
-			<td>Ngoding, Bermain Basket, Mendaki Gunung, Bermain Game, Membaca Buku</td>
-		</tr>
-	</table>
+<div class="container">
+        <div class="profil">
+            <h1>Biodata Diri</h1>
+</body>
+</html>
+<?php
+        echo "<table width='1000'>";
+        echo "<tr>";
+                echo "<th width='150'>";
+                echo "<b>";
+                echo "Nama";
+                echo "</b>";
+                echo "</th>";
+                echo "<td width='750'>";
+                echo "Fadly Indra Agustin";
+                echo "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+                echo "<th width='150'>";
+                echo "<b>";
+                echo "TTL";
+                echo "</b>";
+                echo "</th>";
+                echo "<td width='750'>";
+                echo "Batusangkar, 06 Agustus 2001";
+                echo "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+                echo "<th width='150'>";
+                echo "<b>";
+                echo "Jenis Kelamin";
+                echo "</b>";
+                echo "</th>";
+                echo "<td width='750'>";
+                echo "Laki-laki";
+                echo "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+                echo "<th width='150'>";
+                echo "<b>";
+                echo "Alamat";
+                echo "</b>";
+                echo "</th>";
+                echo "<td width='750'>";
+                echo "Riau, Kota Pekanbaru, Kecamatan Tuah Madani, Kelurahan Sialangmunggu, Jl. Pendidikan, Gang Tauhid II.";
+                echo "</td>";
+        echo "</tr>";
+        
+        echo "<tr>";
+                echo "<th width='150'>";
+                echo "<b>";
+                echo "Foto";
+                echo "</th>";
+                echo "</b>";
+                echo "<td width='750'>";
+                echo '<img src="../FSWD1-arkatama-php/assets/Foto.jpg" alt="">';
+                echo "</td>";
+        echo "</tr>";
+        
+        echo "<tr>";
+                echo "<th width='100'>";
+                echo "<b>";
+                echo "Riwayat Pendidikan";
+                echo "</b>";
+                echo "</th>";
+                echo "<td width='717'>";
+                echo "<ul>";
+                echo "<li>SDN 21 Tanjung Bonai (2008-2014)</li>";
+                echo "<li>SMPN 03 Lintau Buo Utara (2014-2017)</li>";
+                echo "<li>SMAN 01 Lintau Buo Utara (2017-2020)</li>";
+                echo "<li>STMIK AMIK RIAU, Fakultas Teknik Informatika (2020-sekarang)</li>";
+                echo "</ul>";
+                echo "</td>";
+        echo "</tr>";
+        
+        echo "<tr>";
+                echo "<th width='100'>";
+                echo "<b>";
+                echo "Hobi";
+                echo "</b>";
+                echo "</th>";
+                echo "<td width='254'>";
+                echo "Ngoding, Bermain Basket, Mendaki Gunung, Bermain Game, Membaca Buku";
+                echo "</td>";
+        echo "</tr>";
+        echo "</table>";
+        ?>
+
+    </div>
 </body>
 </html>
